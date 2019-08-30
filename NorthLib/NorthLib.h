f03db5374417d3d23822c85630eed465a6e6ba85
Norbert Thies <norbert@taz.de>
2019-08-30 17:40:44 +0200

    Initial Commit

---
+++ NorthLib/NorthLib.h
@@ -0,0 +1,19 @@
+//
+//  NorthLib.h
+//  NorthLib
+//
+//  Created by Norbert Thies on 30.08.19.
+//  Copyright © 2019 Norbert Thies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NorthLib.
+FOUNDATION_EXPORT double NorthLibVersionNumber;
+
+//! Project version string for NorthLib.
+FOUNDATION_EXPORT const unsigned char NorthLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NorthLib/PublicHeader.h>
+
+
