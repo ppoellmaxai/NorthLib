401439f6dafea95d0324e57d2ddea9cd6911de8e
Norbert Thies <norbert@taz.de>
2020-04-30 09:11:24 +0200

    Cosmetic extensions to Notification added

---
+++ NorthLib/Extensions/Notification.swift
@@ -0,0 +1,40 @@
+//
+//  Notification.swift
+//
+//  Created by Norbert Thies on 27.04.20.
+//  Copyright © 2020 Norbert Thies. All rights reserved.
+//
+
+import Foundation
+
+public extension Notification {
+  
+  typealias Observer = NSObjectProtocol?
+  
+  /// Send Notification
+  static func send(_ message: String, object: Any? = nil) {
+    let nn = NSNotification.Name(message)
+    let notification = Notification(name: nn, object: object)
+    NotificationCenter.default.post(notification)
+  }
+  
+  /// Receive Notification on main thread and return observer object
+  @discardableResult
+  static func receive(_ message: String, closure: @escaping (Any?)->()) 
+    -> Observer {
+    let nn = NSNotification.Name(message)
+    let observer = NotificationCenter.default.addObserver(forName: nn, 
+                     object: nil, queue: nil) { notification in
+      closure(notification.object)
+    }
+    return observer
+  }
+  
+  /// Remove observer
+  static func remove(observer: Observer) {
+    if let observer = observer {
+      NotificationCenter.default.removeObserver(observer)
+    }
+  }
+  
+} // Notification Extensions
